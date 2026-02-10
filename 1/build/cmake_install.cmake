6821122fa5e81151cddfd2e37cc74da840600ccf
Nekitai228 <nekitaikapov@gmail.com>
2026-02-10 08:19:44 +0000

    Update in server

--- 1/build/cmake_install.cmake
+++ 1/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/iakup/GitHub/parallelism/parallelism_theory_Rebrikov_24942/1
+# Install script for directory: /home/n.rebrikov/parallelism_theory_Rebrikov_24942/1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/iakup/GitHub/parallelism/parallelism_theory_Rebrikov_24942/1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/n.rebrikov/parallelism_theory_Rebrikov_24942/1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
